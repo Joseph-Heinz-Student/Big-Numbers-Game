d42e7fbd7fc43090f1c157717aa250eac26bf1e4
Qrexx <smeelyoinions@gmail.com>
2024-02-20 13:48:17 -0500

    hotfix 1 // Feb 20 1:47PM
    Fixed error with players that have no numbers per second getting a substring error
    loc: render(game);

--- src/render.js
+++ src/render.js
@@ -1,5 +1,9 @@
 const render = (game) =>{
-    let number_per_second = [getNumberPerSecond(game).toString().split(".")[0],getNumberPerSecond(game).toString().split(".")[1].substring(0,1)].join(".");
+    if(getNumberPerSecond(game).toString().split(".").length > 1){
+        let number_per_second = [getNumberPerSecond(game).toString().split(".")[0],getNumberPerSecond(game).toString().split(".")[1].substring(0,1)].join(".");
+    }else{
+        let number_per_second = getNumberPerSecond(game).toString().split(".")[0];
+    }
     document.title = Game.number.toString().split(".")[0];
     statsDOM.innerHTML = `Number: ${numberformat.format(game.number,{sigfigs:3})} | Big Number: ${numberformat.format(game.big_number,{sigfigs:3})} | Big Number Price: ${numberformat.format(getBigNumberPrice(game))} <strong>N</strong> | Big Number Multiplier: ${numberformat.format(getBigNumberMult(game),{sigfigs:4})}`;
     stats2DOM.innerHTML = `Essence: ${numberformat.format(game.essence,{sigfigs:3})} | Essence cost: ${numberformat.format(getEssenceCost(game))} <strong>BN</strong> | Auto Numberers: ${game.auto_numbers} | Auto Numberer Price: ${getAutoNumberCost(game)} <strong>E</strong> | Number Per Second: ${number_per_second}`;
